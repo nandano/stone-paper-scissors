1e1e752be126e43e3e214ac28c01eca9967737a7
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-26 22:28:19 +0530

    Add loop to play the game without manual restart

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -1,23 +1,28 @@
 import random
 
 choice_list = ['stone', 'paper', 'scissors']
-random_number = random.randint(0, 2)
-user_choice = input("Enter 'stone', 'paper' or 'scissors': ").lower()
-computer_choice = choice_list[random_number]
-print(f"You chose: {user_choice}.")
-print(f"Computer chose: {computer_choice}.")
 
-if user_choice == computer_choice:
-    print("It's a tie.")
+while True:
+    user_choice = input("Enter 'stone', 'paper' or 'scissors': (Enter 'q' to exit): ").lower()
+    if user_choice == 'q':
+        break
+    
+    random_number = random.randint(0, 2)
+    computer_choice = choice_list[random_number]
+    print(f"You chose: {user_choice}.")
+    print(f"Computer chose: {computer_choice}.")
 
-elif user_choice == 'stone' and computer_choice == 'scissors':
-    print("You win.")
+    if user_choice == computer_choice:
+        print("It's a tie.")
 
-elif user_choice == 'paper' and computer_choice == 'stone':
-    print("You win.")
+    elif user_choice == 'stone' and computer_choice == 'scissors':
+        print("You win.")
 
-elif user_choice == 'scissors' and computer_choice == 'paper':
-    print("You win.")
+    elif user_choice == 'paper' and computer_choice == 'stone':
+        print("You win.")
 
-else:
-    print("You lose.")
+    elif user_choice == 'scissors' and computer_choice == 'paper':
+        print("You win.")
+
+    else:
+        print("You lose.")
